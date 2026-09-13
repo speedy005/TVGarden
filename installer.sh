596da0f82bc0b54b249745908a4fcadb7dfdc661
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 18:04:10 +0200

    Update TVGarden download URL in installer.sh

--- installer.sh
+++ installer.sh
@@ -110,7 +110,7 @@ if [ "$OSTYPE" = "OE" ]; then
 fi
 
 echo "Downloading TVGarden..."
-wget --no-check-certificate 'https://github.com/Belfagor2005/TVGarden/archive/refs/heads/main.tar.gz' -O "$FILEPATH"
+wget --no-check-certificate 'https://github.com/speedy005/TVGarden/archive/refs/heads/main.tar.gz' -O "$FILEPATH"
 if [ $? -ne 0 ]; then
     echo "Failed to download TVGarden package!"
     cleanup
