6414cf54b2fc0466ebb3b485eaac01e0110dca29
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 17:50:30 +0200

    Update installer script for version 2.7

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # TV Garden Installer for Enigma2
-# wget -q --no-check-certificate "https://raw.githubusercontent.com/Belfagor2005/TVGarden/main/installer.sh" -O - | /bin/sh
+# wget -q --no-check-certificate "https://raw.githubusercontent.com/speedy005/TVGarden/master/installer.sh" -O - | /bin/sh
 
-version='2.6'
+version='2.7'
 echo "TVGarden Version: $version"
 echo "Changelog:"
 echo "- Add youtube streaming on player"
