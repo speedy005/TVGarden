09ed4f15555548103cf2bd90996c1352d5606f7f
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 17:50:50 +0200

    Update TVGarden plugin version to 2.7

--- usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
@@ -10,7 +10,7 @@ from Components.Language import language
 import gettext
 
 PLUGIN_NAME = "TVGarden"
-__version__ = "2.6"
+__version__ = "2.7"
 PLUGIN_VERSION = __version__
 PLUGIN_PATH = resolveFilename(
     SCOPE_PLUGINS,
