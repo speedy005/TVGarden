c678353e5ae23b18c381916a971ccfe22593bfcf
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-16 16:58:45 +0200

    Update __init__.py

--- usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
@@ -10,7 +10,7 @@ from Components.Language import language
 import gettext
 
 PLUGIN_NAME = "TVGarden"
-__version__ = "3.1"
+__version__ = "3.2"
 PLUGIN_VERSION = __version__
 PLUGIN_PATH = resolveFilename(
     SCOPE_PLUGINS,
