75f173fa30d50fa554716dd5e6c80e5ee5ac4eac
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-16 17:30:54 +0200

    Add files via upload

--- usr/lib/enigma2/python/Plugins/Extensions/TVGarden/helpers.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVGarden/helpers.py
@@ -144,27 +144,27 @@ IMAGES_PATH = join(PLUGIN_PATH, "images", RESOLUTION_TYPE)
 DEFAULT_SKIN_PATH = join(PLUGIN_PATH, "skin", "hd")
 DEFAULT_IMAGES_PATH = join(PLUGIN_PATH, "images", "hd")
 
-REPO_BASE = "https://raw.githubusercontent.com/OwnerPlugins/famelack-data/main"
+REPO_BASE = "https://raw.githubusercontent.com/speedy005/famelack-data/main"
 
 
 def get_metadata_url():
-    return "https://raw.githubusercontent.com/OwnerPlugins/famelack-data/main/tv/raw/countries_metadata.json"
+    return "https://raw.githubusercontent.com/speedy005/famelack-data/main/tv/raw/countries_metadata.json"
 
 
 def get_country_url(country_code):
-    return "https://raw.githubusercontent.com/OwnerPlugins/famelack-data/main/tv/raw/countries/%s.json" % country_code.lower()
+    return "https://raw.githubusercontent.com/speddy005/famelack-data/main/tv/raw/countries/%s.json" % country_code.lower()
 
 
 def get_category_url(category_id):
-    return "https://raw.githubusercontent.com/OwnerPlugins/famelack-data/main/tv/raw/categories/%s.json" % category_id
+    return "https://raw.githubusercontent.com/speedy005/famelack-data/main/tv/raw/categories/%s.json" % category_id
 
 
 def get_categories_url():
-    return "https://api.github.com/repos/OwnerPlugins/famelack-data/contents/tv/raw/categories"
+    return "https://api.github.com/repos/speedy005/famelack-data/contents/tv/raw/categories"
 
 
 def get_all_channels_url():
-    return "https://raw.githubusercontent.com/OwnerPlugins/famelack-data/main/tv/raw/categories/all.json"
+    return "https://raw.githubusercontent.com/speedy005/famelack-data/main/tv/raw/categories/all.json"
 
 
 def get_flag_url(country_code, size=80):
